ea104a6d7b33e4010a6d89db61430fd1530d7813
Anthony Lee <ant.lee813@gmail.com>
2018-01-21 23:49:31 -0800

    added js file

---
+++ popup.js
@@ -0,0 +1,10 @@
+function loadUrls(){
+// fetch urls from textarea and load them
+}
+
+function saveUrls(){
+  // fetch urls and save in chrome storage
+}
+
+document.getElementById('button').addEventListener('click', loadUrls);
+document.getElementById('button').addEventListener('click', saveUrls);
